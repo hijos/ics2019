7cc903b9d46f2b9bd6e588215c4ac0aeba86de4a
tracer-ics2019 <tracer@njuics.org>
2021-01-07 21:30:21 +0800

    >  run U201714759 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:30:21 up 1 day,  3:01,  1 user,  load average: 0.02, 0.07, 0.06 e5b932a76ffdae838c68424f83299565477bd24c

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -42,85 +42,76 @@ int strcmp(const char* s1, const char* s2) {
   while (s1[i] != 0 && s1[i] == s2[i]) {
     i++;
   }
-  return s1[i] - s2[i];
+  return ((unsigned char *) s1)[i] - ((unsigned char *) s2)[i];
 }
 
-// int strncmp(const char* s1, const char* s2, size_t n) {
-//   int i = 0;
-//   while (s1[i] != 0 && s1[i] == s2[i]) {
-//     i++;
-//     if (i == n) {
-//       return 0; 
-//     }
-//   }
-//   return ((unsigned char *) s1)[i] - ((unsigned char *) s2)[i];
-  
-// }
-
-// void* memset(void* v,int c,size_t n) {
-//   for(int i = 0; i < n;i++)
-//     ((unsigned char *)v)[i] = (unsigned char)c;
-//   return v;
-// }
-
-// void* memcpy(void* out, const void* in, size_t n) {
-//   for(int i = 0; i < n; i++)
-//     ((unsigned char*)out)[i] = ((unsigned char*)in)[i];
-//   return out;
-// }
-
-// int memcmp(const void* s1, const void* s2, size_t n){
-//   for(int i = 0; i < n; i++){
-//       if(((unsigned char*)s1)[i] != ((unsigned char*)s2)[i]){
-//           return ((unsigned char*)s1)[i] - ((unsigned char*)s2)[i];
-//       }
-//   }
-//   return 0;
-// }
-
-
-// int strcmp(const char* s1, const char* s2) {
-//     int i = 0;
-//     for(;s1[i];i++){
-//         if(s1[i]!=s2[i])
-//             break;
-//     }
-//   return (s1[i]-s2[i]);
-// }
-
 int strncmp(const char* s1, const char* s2, size_t n) {
-    int i = 0;
-    for(;s1[i] && (i<n);i++){
-        if(s1[i]!=s2[i])
-            break;
+  int i = 0;
+  while (s1[i] != 0 && s1[i] == s2[i]) {
+    i++;
+    if (i == n) {
+      return 0; 
     }
-    if(i==n)
-        return 0;
-    return (s1[i]-s2[i]);
+  }
+  return ((unsigned char *) s1)[i] - ((unsigned char *) s2)[i];
+  
 }
 
 void* memset(void* v,int c,size_t n) {
-    for(int i = 0; i < n;i++){
-        ((char *)v)[i] = (char)c;
-    }
-    return v;
+  for(int i = 0; i < n;i++)
+    ((unsigned char *)v)[i] = (unsigned char)c;
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-    for(int i = 0; i < n; i++){
-        ((char*)out)[i] = ((char*)in)[i];
-    }
-    return out;
+  for(int i = 0; i < n; i++)
+    ((unsigned char*)out)[i] = ((unsigned char*)in)[i];
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
-    for(int i = 0; i < n; i++){
-        if(((char*)s1)[i] != ((char*)s2)[i]){
-            return ((char*)s1)[i] - ((char*)s2)[i];
-        }
-    }
-    return 0;
+  for(int i = 0; i < n; i++){
+      if(((unsigned char*)s1)[i] != ((unsigned char*)s2)[i]){
+          return ((unsigned char*)s1)[i] - ((unsigned char*)s2)[i];
+      }
+  }
+  return 0;
 }
+
+
+// int strncmp(const char* s1, const char* s2, size_t n) {
+//     int i = 0;
+//     for(;s1[i] && (i<n);i++){
+//         if(s1[i]!=s2[i])
+//             break;
+//     }
+//     if(i==n)
+//         return 0;
+//     return (s1[i]-s2[i]);
+// }
+
+// void* memset(void* v,int c,size_t n) {
+//     for(int i = 0; i < n;i++){
+//         ((char *)v)[i] = (char)c;
+//     }
+//     return v;
+// }
+
+// void* memcpy(void* out, const void* in, size_t n) {
+//     for(int i = 0; i < n; i++){
+//         ((char*)out)[i] = ((char*)in)[i];
+//     }
+//     return out;
+// }
+
+// int memcmp(const void* s1, const void* s2, size_t n){
+//     for(int i = 0; i < n; i++){
+//         if(((char*)s1)[i] != ((char*)s2)[i]){
+//             return ((char*)s1)[i] - ((char*)s2)[i];
+//         }
+//     }
+//     return 0;
+// }
 
 #endif
 
