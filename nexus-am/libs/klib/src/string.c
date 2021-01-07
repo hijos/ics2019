5f5a3bb346082a7aab3f819a84831b1119b47574
tracer-ics2019 <tracer@njuics.org>
2021-01-07 21:19:38 +0800

    >  compile U201714759 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:19:38 up 1 day,  2:50,  1 user,  load average: 0.04, 0.05, 0.06 cc7a0c7cccfb8660c85a86a011b2236f89ffccad

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,38 +3,78 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  int i;
+  for(i=0;s[i];i++){}
+  return i;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  int i = 0;
+  while(src[i]){
+    dst[i] = src[i];
+    i++;
+  }
+  assert(src[i] == '\0');
+  dst[i] = 0;
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  int i = 0;
+  while(src[i] && (i<n)){
+    dst[i] = src[i];
+    i++;
+  }
+  while(i<n){
+    dst[i] = 0;
+    i++;
+  }
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  strcpy((char*)(dst+strlen(dst)), src);
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  int i;
+  while (s1[i] != 0 && s1[i] == s2[i]) {
+    i++;
+  }
+  return ((unsigned char *) s1)[i] - ((unsigned char *) s2)[i];
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+  int i = 0;
+  while (s1[i] != 0 && s1[i] == s2[i]) {
+    i++;
+    if (i == n) {
+      return 0; 
+    }
+  }
+  return ((unsigned char *) s1)[i] - ((unsigned char *) s2)[i];
+  
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  for(int i = 0; i < n;i++)
+    ((unsigned char *)v)[i] = (unsigned char)c;
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  for(int i = 0; i < n; i++)
+    ((unsigned char*)out)[i] = ((unsigned char*)in)[i];
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
+  for(int i = 0; i < n; i++){
+      if(((unsigned char*)s1)[i] != ((unsigned char*)s2)[i]){
+          return ((unsigned char*)s1)[i] - ((unsigned char*)s2)[i];
+      }
+  }
   return 0;
 }
 
