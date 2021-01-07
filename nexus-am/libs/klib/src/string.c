d5a5665418f89a529736948d477913704ba6a4bf
tracer-ics2019 <tracer@njuics.org>
2021-01-07 21:30:32 +0800

    >  run U201714759 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:30:32 up 1 day,  3:01,  1 user,  load average: 0.02, 0.06, 0.06 226f07ee74e87da8a53248c094d6059091ba45

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -78,40 +78,5 @@ int memcmp(const void* s1, const void* s2, size_t n){
   return 0;
 }
 
-
-// int strncmp(const char* s1, const char* s2, size_t n) {
-//     int i = 0;
-//     for(;s1[i] && (i<n);i++){
-//         if(s1[i]!=s2[i])
-//             break;
-//     }
-//     if(i==n)
-//         return 0;
-//     return (s1[i]-s2[i]);
-// }
-
-// void* memset(void* v,int c,size_t n) {
-//     for(int i = 0; i < n;i++){
-//         ((char *)v)[i] = (char)c;
-//     }
-//     return v;
-// }
-
-// void* memcpy(void* out, const void* in, size_t n) {
-//     for(int i = 0; i < n; i++){
-//         ((char*)out)[i] = ((char*)in)[i];
-//     }
-//     return out;
-// }
-
-// int memcmp(const void* s1, const void* s2, size_t n){
-//     for(int i = 0; i < n; i++){
-//         if(((char*)s1)[i] != ((char*)s2)[i]){
-//             return ((char*)s1)[i] - ((char*)s2)[i];
-//         }
-//     }
-//     return 0;
-// }
-
 #endif
 
