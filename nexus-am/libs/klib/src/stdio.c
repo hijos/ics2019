5a9282db4819634aa74382ab28c9245d95d6632a
meili <1019378968@qq.com>
2021-01-08 11:35:10 +0800

    comlete pa2.2

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -1,27 +1,3 @@
-// #include "klib.h"
-// #include <stdarg.h>
-
-// #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
-// int printf(const char *fmt, ...) {
-//   return 0;
-// }
-
-// int vsprintf(char *out, const char *fmt, va_list ap) {
-//   return 0;
-// }
-
-// int sprintf(char *out, const char *fmt, ...) {
-//   return 0;
-// }
-
-// int snprintf(char *out, size_t n, const char *fmt, ...) {
-//   return 0;
-// }
-
-// #endif
-
-
 #include "klib.h"
 #include <stdarg.h>
 
@@ -119,6 +95,22 @@ void my_format(void *putp, putcf putf, const char *fmt, va_list va) {
 
 	}
 }
+
+// int printf(const char *fmt, ...) {
+//   return 0;
+// }
+
+// int vsprintf(char *out, const char *fmt, va_list ap) {
+//   return 0;
+// }
+
+// int sprintf(char *out, const char *fmt, ...) {
+//   return 0;
+// }
+
+// int snprintf(char *out, size_t n, const char *fmt, ...) {
+//   return 0;
+// }
 
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
