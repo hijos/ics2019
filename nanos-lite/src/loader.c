824538c918be988d6c1fde2b2c3baeb11f63fed3
tracer-ics2019 <tracer@njuics.org>
2021-01-10 22:25:44 +0800

    >  run U201714759 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:25:44 up 11:59,  1 user,  load average: 0.27, 0.12, 0.09 47e2e922c9cff29bb608c4a27f15e8be2e647d24

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,35 +16,16 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   // TODO();
   Elf_Ehdr Ehdr;// read the ELF Header
   ramdisk_read(&Ehdr, 0, sizeof(Ehdr));
-  Elf_Phdr Phdr;//read Program Header
-  ramdisk_read(&Phdr, Ehdr.e_phoff, sizeof(Phdr));
-  ramdisk_read((void*)Phdr.p_vaddr, Phdr.p_offset, Phdr.p_filesz);
-  memset((void*)(Phdr.p_vaddr+Phdr.p_filesz),0,(Phdr.p_memsz-Phdr.p_filesz));
+  for (uint16_t i=0; i<Ehdr.e_phnum; i++){
+    Elf_Phdr Phdr;//read Program Header
+    ramdisk_read(&Phdr, Ehdr.e_phoff + i*Ehdr.e_phentsize, sizeof(Phdr));
+    if(Phdr.p_type == PT_LOAD){
+      ramdisk_read((void*)Phdr.p_vaddr, Phdr.p_offset, Phdr.p_filesz);
+      memset((void*)(Phdr.p_vaddr+Phdr.p_filesz),0,(Phdr.p_memsz-Phdr.p_filesz));
+    }
+  }
 
   return Ehdr.e_entry;
-
-  // Elf_Ehdr Ehdr;
-  // Elf_Phdr Phdr;
-  // int fd = fs_open(filename,0,0);
-  // assert(fd!=-1);
-  // fs_read(fd,&Ehdr,sizeof(Elf_Ehdr));
-  // fs_lseek(fd,Ehdr.e_phoff,SEEK_SET);
-  // size_t open_offset;
-
-  // for (uint16_t i=0; i<Ehdr.e_phnum; i++){
-
-  //   fs_read(fd,&Phdr,sizeof(Elf_Phdr));
-  //   open_offset = fs_open_offset(fd);
-  //   if(Phdr.p_type == PT_LOAD){
-  //     fs_lseek(fd,Phdr.p_offset,SEEK_SET);
-  //     fs_read(fd,(void *)Phdr.p_vaddr,Phdr.p_filesz);
-  //     memset((void*)(Phdr.p_vaddr+Phdr.p_filesz),0,(Phdr.p_memsz-Phdr.p_filesz));
-  //   }
-  //   fs_lseek(fd,open_offset,SEEK_SET);
-
-  // }
-  // fs_close(fd);
-  // return Ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
