88a19e370a2704dcd2f5126da68cce112a26ab62
tracer-ics2019 <tracer@njuics.org>
2021-01-10 16:57:07 +0800

    >  run U201714759 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:57:07 up  6:30,  1 user,  load average: 0.11, 0.10, 0.08 2825fa2f3c820e235a003cce6f3d9498ef474dae

--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -8,16 +8,30 @@ _Context* __am_irq_handle(_Context *c) {
   _Context *next = c;
 
   // output the context
-  for(int i = 1; i < 32;i++){
-      printf("gpr[%d]:%d\n",i,c->gpr[i]);
-  }
-  printf("cause:%d\n", c->cause);
-  printf("status:%d\n", c->status);
-  printf("epc:0x%x\n", c->epc);
+  // for(int i = 1; i < 32;i++){
+  //     printf("gpr[%d]:%d\n",i,c->gpr[i]);
+  // }
+  // printf("cause:%d\n", c->cause);
+  // printf("status:%d\n", c->status);
+  // printf("epc:0x%x\n", c->epc);
 
   if (user_handler) {
     _Event ev = {0};
     switch (c->cause) {
+      case -1:
+          ev.event = _EVENT_YIELD;
+          break;
+      case 0:
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 7:
+      case 8:
+      case 9:
+      case 13:
+          ev.event = _EVENT_SYSCALL;
+          break;
       default: ev.event = _EVENT_ERROR; break;
     }
 
