d4f7922bb2184d1d9822c1e5b8df840901d6d955
tracer-ics2019 <tracer@njuics.org>
2021-01-12 16:04:07 +0800

    >  run U201714759 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:04:07 up 2 days,  5:37,  1 user,  load average: 0.03, 0.06, 0.01 df4134ad1c638d66320af4e23b038dea5856b35a

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,31 +6,31 @@
 static int programBrk;
 
 int do_write(int fd, const void*buf, size_t count){
-  if(fd==1 || fd==2){
-    for(int i = 0;i < count;i++){
-        _putc(((char*)buf)[i]);
-    }
-    return count;
-  }
-  return 0;
-  // return fs_write(fd, buf, count);
+  // if(fd==1 || fd==2){
+  //   for(int i = 0;i < count;i++){
+  //       _putc(((char*)buf)[i]);
+  //   }
+  //   return count;
+  // }
+  // return 0;
+  return fs_write(fd, buf, count);
 }
 
-// int do_open(const char*path, int flags, int mode){
-//   return fs_open(path, flags, mode);
-// }
+int do_open(const char*path, int flags, int mode){
+  return fs_open(path, flags, mode);
+}
 
-// int do_close(int fd){
-//   return fs_close(fd);
-// }
+int do_close(int fd){
+  return fs_close(fd);
+}
 
-// int do_read(int fd, void*buf, size_t count){
-//   return fs_read(fd, buf, count);
-// }
+int do_read(int fd, void*buf, size_t count){
+  return fs_read(fd, buf, count);
+}
 
-// size_t do_lseek(int fd, size_t offset, int whence){
-//   return fs_lseek(fd, offset, whence);
-// }
+size_t do_lseek(int fd, size_t offset, int whence){
+  return fs_lseek(fd, offset, whence);
+}
 
 int do_brk(int addr){
   programBrk = addr;
@@ -56,18 +56,18 @@ _Context* do_syscall(_Context *c) {
     case SYS_write:
         c->GPRx = do_write(a[1], (void*)(a[2]), a[3]);
         break;
-    // case SYS_read:
-    //     c->GPRx = do_read(a[1], (void*)(a[2]), a[3]);
-    //     break;
-    // case SYS_lseek:
-    //     c->GPRx = do_lseek(a[1], a[2], a[3]);
-    //     break;
-    // case SYS_open:
-    //     c->GPRx = do_open((const char *)a[1], a[2], a[3]);
-    //     break;
-    // case SYS_close:
-    //     c->GPRx = do_close(a[1]);
-    //     break;
+    case SYS_read:
+        c->GPRx = do_read(a[1], (void*)(a[2]), a[3]);
+        break;
+    case SYS_lseek:
+        c->GPRx = do_lseek(a[1], a[2], a[3]);
+        break;
+    case SYS_open:
+        c->GPRx = do_open((const char *)a[1], a[2], a[3]);
+        break;
+    case SYS_close:
+        c->GPRx = do_close(a[1]);
+        break;
     case SYS_brk:
         c->GPRx = do_brk(a[1]);
         break;
