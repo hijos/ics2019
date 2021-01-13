5f5f324e206749fbd51493c7630ed5e056be9e7c
tracer-ics2019 <tracer@njuics.org>
2021-01-13 17:35:27 +0800

    >  compile U201714759 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:35:27 up  4:19,  1 user,  load average: 0.47, 0.55, 0.75 48e5327863f1f3ec7ba5d65d268e8f8bc209551e

---
+++ nemu/srcml/monitor/debug/ui.c
@@ -0,0 +1,229 @@
+#include "monitor/monitor.h"
+#include "monitor/expr.h"
+#include "monitor/watchpoint.h"
+#include "nemu.h"
+
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+void cpu_exec(uint64_t);
+
+extern void isa_reg_display();
+
+extern void difftest_attach();
+extern void difftest_detach();
+
+extern WP *head;
+
+/* We use the `readline' library to provide more flexibility to read from stdin. */
+static char* rl_gets() {
+  static char *line_read = NULL;
+
+  if (line_read) {
+    free(line_read);
+    line_read = NULL;
+  }
+
+  line_read = readline("(nemu) ");
+
+  if (line_read && *line_read) {
+    add_history(line_read);
+  }
+
+  return line_read;
+}
+
+static int cmd_c(char *args) {
+  cpu_exec(-1);
+  return 0;
+}
+
+static int cmd_q(char *args) {
+  return -1;
+}
+
+static int cmd_si(char *args){
+  int lineNum = 1;
+  if(args){
+    sscanf(args, "%d", &lineNum);
+  }
+  cpu_exec(lineNum);
+  return 0;
+}
+
+static int cmd_info(char *args){
+  if(args){
+    if(args[0] == 'r'){
+      isa_reg_display();
+    }
+    else if(args[0] == 'w'){
+      printf("%-6s%-20s%-10s\n","Num", "Expression", "Result");
+      for(WP* tmp = head;tmp;tmp = tmp->next){
+        printf("%-6d%-20s%-6d\n", tmp->NO, tmp->wp_expr, tmp->last_value);
+      }
+      return 0;
+    }
+    else{
+      printf("Wrong argument!\n");
+    }
+  }
+  else{
+    printf("Lack argument!\n");
+  }
+  return 0;
+}
+
+static int cmd_x(char *args){
+  int number, index;
+  number = index = -1;
+  sscanf(args, "%d 0x%x", &number, &index);
+  if(index == -1 || number <= 0 ){
+    printf("Wrong argument!\n");
+    return 0;
+  }
+  for(int i = 0; i < number; i++){
+    if(i%4 == 0){
+      printf("0x%-10x:", index + i*4);
+    }
+    printf("0x%-10x", isa_vaddr_read(index + i*4, 4));
+    if( ((i-3)%4 == 0) && (i != number-1))
+      printf("\n");
+  }
+  printf("\n");
+  return 0;
+}
+
+static int cmd_p(char *args){
+  bool success;
+  int res;
+  res = expr(args, &success);
+  if(!success){
+    printf("Wrong expression!\n");
+    return 0;
+  }
+  printf("0x%x %d\n", res, res);
+  return 0;
+}
+
+static int cmd_w(char *args){
+  bool success;
+  int res;
+  res = expr(args, &success);
+  if(!success){
+    printf("Wrong expression!\n");
+    return 0;
+  }
+  WP *wp = new_wp();
+  wp->last_value = res;
+  strcpy(wp->wp_expr, args);
+  return 0;
+}
+
+static int cmd_d(char *args){
+  int number;
+  sscanf(args, "%d", &number);
+  for(WP* tmp = head; tmp; tmp = tmp->next){
+    if(tmp->NO == number){
+      free_wp(tmp);
+      return 0;
+    }
+  }
+  return 0;
+}
+
+static int cmd_attach(char *args){
+    difftest_attach();
+    return 0;
+}
+
+static int cmd_detach(char *args){
+    difftest_detach();
+    return 0;
+}
+
+static int cmd_help(char *args);
+
+static struct {
+  char *name;
+  char *description;
+  int (*handler) (char *);
+} cmd_table [] = {
+  { "help", "Display informations about all supported commands", cmd_help },
+  { "c", "Continue the execution of the program", cmd_c },
+  { "q", "Exit NEMU", cmd_q },
+
+  /* TODO: Add more commands */
+  { "si", "Execute N steps", cmd_si },
+  { "info", "Show the information of register", cmd_info },
+  { "x", "Show the details of memory", cmd_x },
+  { "p", "Print the result of an expression", cmd_p },
+  { "w", "Set watchpoint", cmd_w },
+  { "d", "Delete watchpoint", cmd_d },
+  { "detach", "Quit difftest mode", cmd_detach},
+  { "attach", "Dive into difftest mode", cmd_attach},
+
+};
+
+#define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
+
+static int cmd_help(char *args) {
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+  int i;
+
+  if (arg == NULL) {
+    /* no argument given */
+    for (i = 0; i < NR_CMD; i ++) {
+      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+    }
+  }
+  else {
+    for (i = 0; i < NR_CMD; i ++) {
+      if (strcmp(arg, cmd_table[i].name) == 0) {
+        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+        return 0;
+      }
+    }
+    printf("Unknown command '%s'\n", arg);
+  }
+  return 0;
+}
+
+void ui_mainloop(int is_batch_mode) {
+  if (is_batch_mode) {
+    cmd_c(NULL);
+    return;
+  }
+
+  for (char *str; (str = rl_gets()) != NULL; ) {
+    char *str_end = str + strlen(str);
+
+    /* extract the first token as the command */
+    char *cmd = strtok(str, " ");
+    if (cmd == NULL) { continue; }
+
+    /* treat the remaining string as the arguments,
+     * which may need further parsing
+     */
+    char *args = cmd + strlen(cmd) + 1;
+    if (args >= str_end) {
+      args = NULL;
+    }
+
+#ifdef HAS_IOE
+    extern void sdl_clear_event_queue(void);
+    sdl_clear_event_queue();
+#endif
+
+    int i;
+    for (i = 0; i < NR_CMD; i ++) {
+      if (strcmp(cmd, cmd_table[i].name) == 0) {
+        if (cmd_table[i].handler(args) < 0) { return; }
+        break;
+      }
+    }
+
+    if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+  }
+}
