3cfe969f03cf6b96c57dc7f1c5d60849176aee9f
tracer-ics2019 <tracer@njuics.org>
2021-01-13 18:44:47 +0800

    >  compile U201714759 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:44:47 up  5:28,  1 user,  load average: 0.79, 0.32, 0.24 d825fd51261bf939189177da6a70b13831c7084

--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -2,12 +2,12 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  for (int i = 0; i < 32; i++) {
-    if (ref_r->gpr[i]._32 != reg_l(i)) {
+  for(int i = 0; i < 32;i++){
+    if(reg_l(i)!=ref_r->gpr[i]._32)
       return false;
-    }
   }
-  // return pc == ref_r->pc;
+  // if(ref_r->pc != pc)
+  //   return false;
   return true;
 }
 
