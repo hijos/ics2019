00767600802feda8b93e76440a517cf1cf4f6897
tracer-ics2019 <tracer@njuics.org>
2021-01-14 00:03:42 +0800

    >  compile U201714759 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:03:42 up 10:47,  1 user,  load average: 0.03, 0.06, 0.08 4b5b81c58566af7b1a7ef66c7a36515239a12837

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -27,7 +27,7 @@ static struct rule {
    */
 
   {"\\$[\\$0-9a-zA-z]+|pc", TK_REG}, //registers: Can be more specific
-  {"0x[0-9a-fA-F]+", TK_HEXNUM},  //Hex number: Must before number
+  {"0(x|X)[0-9a-fA-F]+", TK_HEXNUM},  //Hex number: Must before number
   {"[0-9]+", TK_NUM}, //numbers
   {"-", '-'},   // minus
   {"\\*", '*'}, // multiply
